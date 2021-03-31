397bbc039bd0331ebe3f16c179a60a7556de985d
ododonov <o.dodonov@avtomatika.kz>
2021-03-31 09:22:46 +0600

    auth fix 2

--- scripts/password_check.php
+++ scripts/password_check.php
@@ -16,10 +16,10 @@ if ($mysqli->connect_errno) {
     echo "Error MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 
-$res = $mysqli->query("SELECT * FROM users WHERE name = $login");
+$res = $mysqli->query("SELECT * FROM `users` WHERE `name` = $login");
 $info = mysqli_fetch_array($res);
 if ($login == $info['name'] && $password == $info['password'])
-    {
+  {
     if ($info['is_admin'] == 1)
     {
       echo "admin";
@@ -28,7 +28,7 @@ if ($login == $info['name'] && $password == $info['password'])
     {
       echo "user";
     }
-}
+  }
 else
 {
     echo "invalid";
