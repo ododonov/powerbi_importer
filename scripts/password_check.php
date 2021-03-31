0ec63016629807af6f300547141ebb85e6a66661
ododonov <o.dodonov@avtomatika.kz>
2021-03-31 09:27:29 +0600

    auth fix 3

--- scripts/password_check.php
+++ scripts/password_check.php
@@ -15,9 +15,9 @@ $mysqli = new mysqli($servername, $username, $password, $database, 3306);
 if ($mysqli->connect_errno) {
     echo "Error MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
-
-$res = $mysqli->query("SELECT * FROM `users` WHERE `name` = $login");
-$info = mysqli_fetch_array($res);
+$query = "SELECT * FROM users WHERE name = $login"
+$result = $mysql_query($query) or die(mysql_eror);
+$info = mysqli_fetch_array($result);
 if ($login == $info['name'] && $password == $info['password'])
   {
     if ($info['is_admin'] == 1)
